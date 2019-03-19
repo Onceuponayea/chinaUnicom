fa4ca6cbc5bc72b66e36b7c119a2cbf8c9a302e2
cmzzto <cmzzto@DESKTOP-IUE40UG>
2019-03-19 23:41:10 +0800

    cmzzto

--- chinaunicom/src/com/great/dao/TestDao.java
+++ chinaunicom/src/com/great/dao/TestDao.java
@@ -1,9 +1,17 @@
 package com.great.dao;
 
+import java.util.List;
+
+import com.great.entity.Consumrecord;
 import com.great.entity.Emp;
 
 public interface TestDao {
 
-	public void addEmp(Emp emp);
-	public void deleteEmp(int id);
+	public int addConsumrecord (Consumrecord con);//增加消费记录  cyl
+	
+	public int deleteLastInfo(int consum_id);  //根据id删除
+	
+	public int updateInfo(Consumrecord con);//根据id更新 对象的信息 传进来的是对象
+	
+	public List<Consumrecord> findAll();//查询所有消费记录
 }
